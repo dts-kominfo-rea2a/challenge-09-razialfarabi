2905d74db38c1ebaa834cbf29a41ed419990cf03
Razi Alfarabi <alfarabirazi@gmail.com>
2022-06-20 21:53:51 +0700

    Contact.js - Razi Alfarabi

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,18 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({dataContact}) => {
+    // const dataContact = props.dataContact;
     return (
-        <></>
-    )
+        <div className='contact'>
+            <img src={dataContact.photo} alt={dataContact.name}></img>
+            <div className='detailContact'>
+                <p>{dataContact.phone}</p>
+                <p>{dataContact.name}</p>
+                <p>{dataContact.email}</p>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
