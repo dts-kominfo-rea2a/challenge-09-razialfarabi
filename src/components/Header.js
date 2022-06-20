7c206b6fd14630ea805cb87128a6dc20aa3c5be2
Razi Alfarabi <alfarabirazi@gmail.com>
2022-06-20 20:20:43 +0700

    Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <div className='header'>
+            <h2>Call a Friend</h2>
+            <p style={{ fontWeight: 'bold' }}>your friendly contact app</p>
+            <p>* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *</p>
+        </div>
     )
 }
 
